b438a430da77a5c07acfa6273424e6b60d41e74d
Guillaume Bury <guillaume.bury@gmail.com>
2017-11-20 19:45:10 +0100

    Add parsing to exectuable
    Add the capability to parse to the executable. This uses Dolmen for the
    parsing, and cmdliner to parse command line options. Additionally, Gen
    is also used for clarity of code.

---
+++ src/bin/options.ml
@@ -0,0 +1,155 @@
+
+(* Option types *)
+(* ************************************************************************ *)
+
+type language = Input.language
+
+type input_options = {
+  dir      : string;
+  file     : string;
+  language : language option;
+}
+
+type t = {
+
+  input : input_options;
+
+  (* Time/Memory options *)
+  time_limit  : float;
+  size_limit  : float;
+}
+
+(* Creating option records *)
+(* ************************************************************************ *)
+
+let mk_input_options f language =
+  let dir = Filename.dirname f in
+  let file = Filename.basename f in
+  { dir; file; language; }
+
+let mk input time_limit size_limit =
+  { input; time_limit; size_limit; }
+
+(* Argument converters *)
+(* ************************************************************************ *)
+
+let input = Cmdliner.Arg.enum Input.enum
+
+(* Argument converter for integer with multiplier suffix *)
+(* ************************************************************************ *)
+
+let nb_sec_minute = 60
+let nb_sec_hour = 60 * nb_sec_minute
+let nb_sec_day = 24 * nb_sec_hour
+
+let print_aux suffix fmt n =
+  if n = 0 then ()
+  else Format.fprintf fmt "%d%s" n suffix
+
+let print_time fmt f =
+  let n = int_of_float f in
+  let aux n div = n / div, n mod div in
+  let n_day, n = aux n nb_sec_day in
+  let n_hour, n = aux n nb_sec_hour in
+  let n_min, n = aux n nb_sec_minute in
+  Format.fprintf fmt "%a%a%a%a"
+    (print_aux "d") n_day
+    (print_aux "h") n_hour
+    (print_aux "m") n_min
+    (print_aux "s") n
+
+let parse_time arg =
+  let l = String.length arg in
+  let multiplier m =
+    let arg1 = String.sub arg 0 (l-1) in
+    `Ok (m *. (float_of_string arg1))
+  in
+  assert (l > 0);
+  try
+    match arg.[l-1] with
+    | 's' -> multiplier 1.
+    | 'm' -> multiplier 60.
+    | 'h' -> multiplier 3600.
+    | 'd' -> multiplier 86400.
+    | '0'..'9' -> `Ok (float_of_string arg)
+    | _ -> `Error "bad numeric argument"
+  with Failure _ -> `Error "bad numeric argument"
+
+let print_size fmt f =
+  let n = int_of_float f in
+  let aux n div = n / div, n mod div in
+  let n_tera, n = aux n 1_000_000_000_000 in
+  let n_giga, n = aux n 1_000_000_000 in
+  let n_mega, n = aux n 1_000_000 in
+  let n_kilo, n = aux n 1_000 in
+  Format.fprintf fmt "%a%a%a%a%a"
+    (print_aux "To") n_tera
+    (print_aux "Go") n_giga
+    (print_aux "Mo") n_mega
+    (print_aux "ko") n_kilo
+    (print_aux "") n
+
+let parse_size arg =
+  let l = String.length arg in
+  let multiplier m =
+    let arg1 = String.sub arg 0 (l-1) in
+    `Ok (m *. (float_of_string arg1))
+  in
+  assert (l > 0);
+  try
+    match arg.[l-1] with
+    | 'k' -> multiplier 1e3
+    | 'M' -> multiplier 1e6
+    | 'G' -> multiplier 1e9
+    | 'T' -> multiplier 1e12
+    | '0'..'9' -> `Ok (float_of_string arg)
+    | _ -> `Error "bad numeric argument"
+  with Failure _ -> `Error "bad numeric argument"
+
+let c_time = parse_time, print_time
+let c_size = parse_size, print_size
+
+(* Command terms *)
+(* ************************************************************************ *)
+
+let common_options = "COMMON OPTIONS"
+
+let man = [
+  `S common_options;
+  `P "Common options for the prover";
+]
+
+let info = Cmdliner.Term.(info ~man ~sdocs:common_options ~version:"0.1" "witan")
+
+let input_options =
+  let docs = common_options in
+  let fd =
+    let doc = "Input problem file" in
+    Cmdliner.Arg.(required & pos 0 (some non_dir_file) None & info [] ~docv:"FILE" ~doc)
+  in
+  let language =
+    let doc = Format.asprintf
+        "Set the format for the input file to $(docv) (%s)."
+        (Cmdliner.Arg.doc_alts_enum ~quoted:false Input.enum) in
+    Cmdliner.Arg.(value & opt (some input) None & info ["i"; "input"] ~docs ~docv:"INPUT" ~doc)
+  in
+  Cmdliner.Term.(const mk_input_options $ fd $ language)
+
+let all =
+  let docs = common_options in
+  let time =
+    let doc = {|Stop the program after a time lapse of $(docv).
+                Accepts usual suffixes for durations : s,m,h,d.
+                Without suffix, default to a time in seconds.|} in
+    Cmdliner.Arg.(value & opt c_time 300. & info ["t"; "time"] ~docs ~docv:"TIME" ~doc)
+  in
+  let size =
+    let doc = {|Stop the program if it tries and use more the $(docv) memory space.
+                Accepts usual suffixes for sizes : k,M,G,T.
+                Without suffix, default to a size in octet.|} in
+    Cmdliner.Arg.(value & opt c_size 1_000_000_000. & info ["s"; "size"] ~docs ~docv:"SIZE" ~doc)
+  in
+  Cmdliner.Term.(const mk $ input_options $ time $ size)
+
+
+
--- src/bin/witan.ml
+++ src/bin/witan.ml
@@ -1,5 +1,21 @@
 
-
 let () =
-  Format.printf "Witan !@."
+  (* Parse command line options *)
+  let opts = match Cmdliner.Term.eval (Options.all, Options.info) with
+    | `Version | `Help -> exit 0
+    | `Error `Parse
+    | `Error `Term
+    | `Error `Exn -> exit 1
+    | `Ok opts -> opts
+  in
+  (* Parse input *)
+  let statements = Input.read
+      ?language:Options.(opts.input.language)
+      ~dir:Options.(opts.input.dir)
+      Options.(opts.input.file)
+  in
+  Gen.iter (fun s ->
+      Format.printf "%a@." Dolmen.Statement.print s
+    ) statements;
+  (* TODO: Solve, ^^ *)
 
